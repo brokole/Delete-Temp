7eba780fd270d8acc08d352e01d64687ebe29869
brokole <ajjkole@gmail.com>
2023-02-15 17:21:17 +0100

    Created delete-temp.bat

---
+++ Delete-temp.bat
@@ -0,0 +1,6 @@
+@echo off
+
+echo rd %temp% /s /q md %temp% > "C:\Users\"Username"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\ThorsHammer.bat"
+@echo creating smiting tool for windows 10
+timeout 3 > NUL
+(goto) 2>nul & del "%~f0
